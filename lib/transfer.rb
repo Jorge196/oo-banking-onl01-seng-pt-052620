1981013753491b84c0f3f1317755843484f8f2df
Jorge Ramirez <joramirez96@gmail.com>
2020-06-25 21:48:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
-  attr_accessor :sender, :receiver
+  attr_accessor :sender, :receiver, :status
 
-  def initialize(transfer)
+  def initialize(transfer, sender, receiver)
     @transfer = transfer
     @sender = sender
     @receiver = receiver
